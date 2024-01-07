af07f3b89969a4b9ac6acd1ab04c0bb9ed06a501
Pwhitlock <pwhitlock131@gmail.com>
2024-01-07 14:21:21 -0600

    Finished generateMarkdown.js, next will update readme and test to make sure code works properly

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,9 +9,9 @@ function renderLicenseBadge(license) {
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
   } else if (license === 'GPL 3.0') {
     return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+  } else {
+    return ''; // return an empty string if the license is not recognized
   }
-  
-  return ''; // return an empty string if the license is not recognized
 }
 
 // TODO: Create a function that returns the license link
@@ -20,21 +20,25 @@ function renderLicenseLink(license) {
   if (license === 'None') {
     return '';
   } else if (license === 'MIT') {
-    return '(https://opensource.org/licenses/MIT)'
+    return '[MIT](https://opensource.org/licenses/MIT)';
   } else if (license === 'Apache 2.0') {
-    return '(https://opensource.org/licenses/Apache-2.0)'
+    return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
   } else if (license === 'GPL 3.0') {
-    return '(https://www.gnu.org/licenses/gpl-3.0)'
+    return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
+  } else {
+    return '';
   }
 }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  // TODO: Implement the logic to return the license section of README
-  // This may include additional information about the license and how it can be found.
-  // If there is no license, return an empty string.
-  return '';
+  if (license === 'None') {
+    return '';
+  } else {
+    return `${renderLicenseBadge(license)}${renderLicenseLink(license)}`;
+  }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -68,12 +72,6 @@ ${data.usage}
 ${licenseBadge}
 ${licenseLink}
 ${licenseSection}
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
 
 ## Questions
 For any questions, please contact [${data.github}](https://github.com/${data.github}) via email at ${data.email}.
