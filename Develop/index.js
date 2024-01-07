101165a94b360b8bbfce1a66c6e41d26db687223
Pwhitlock <pwhitlock131@gmail.com>
2024-01-07 13:39:09 -0600

    finished index.js prompts and added validation for questions that needed it

--- Develop/index.js
+++ Develop/index.js
@@ -3,32 +3,73 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown.js');
 const { error } = require('console');
-// TODO: Create an array of questions for user input
-// Add validation to questions?
-const questions = [{
-    type: 'input',
-    message: 'What is your repository called?',
-    name: 'title',
-},
-{
-    type: 'input',
-    message: 'Please give a detailed description of your repository.',
-    name: 'description',
-},
-{
-    type: 'input',
-    message: 'Please add installation instructions if you have any.',
-    name: 'installation',
-},
-    
+
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your repository called?',
+        name: 'title',
+        validate: function (input) {
+            return input !== '' ? true : 'Repo name cannot be empty';
+        },
+    },
+    {
+        type: 'input',
+        message: 'Please give a detailed description of your repository.',
+        name: 'description',
+        validate: function (input) {
+            return input !== '' ? true : 'Description cannot be empty';
+        },
+    },
+    {
+        type: 'input',
+        message: 'Please add installation instructions if you have any.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please add a usage guide.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a liscense.',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None']
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+        validate: function (input) {
+            return input !== '' ? true : 'Please add your username.';
+        },
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+        validate: function (input) {
+            const emailRegex = /\S+@\S+\.\S+/;
+            return emailRegex.test(input) ? true : 'Please enter a valid email address.';
+        },
+        
+    },
 ];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
-    //impiment this function to write the generated readme content to a file
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md succssfully created!');
+        }
+    });
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
     inquirer
         .prompt(questions)
@@ -42,7 +83,7 @@ function init() {
             console.log(`${fileName} created successfully!`);
         })
         .catch((error) => {
-            console.error('Sorry, looks like an error occured during prompts.', error);
+            console.error('Sorry, looks like an error occurred during prompts.', error);
         });
 }
 
